f44bce190e9befb651b2c586546e24a48a5fff1f
ketan-lambat <ketan.lambat@ymail.com>
2019-09-10 10:26:54 +0530

    song_playing_fragment made | build failing

---
+++ Applications/02 Echo/app/src/main/java/com/example/echo/adapters/MainScreenAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.echo.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.echo.R
+import com.example.echo.Songs
+import kotlinx.android.synthetic.main.row_custom_mainscreen_adapter.view.*
+
+class MainScreenAdapter(_songDetails: ArrayList<Songs>, _context: Context) :
+    RecyclerView.Adapter<MainScreenAdapter.MyViewHolder>() {
+
+    var songDetails: ArrayList<Songs>? = null
+    var mContext: Context? = null
+
+    init {
+        this.songDetails = _songDetails
+        this.mContext = _context
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val songObject = songDetails?.get(position)
+        holder.trackTitle?.text = songObject?.songTitle
+        holder.trackArtist?.text = songObject?.artist
+        holder.contentHolder?.setOnClickListener({
+            Toast.makeText(mContext, "Hey " + songObject?.songTitle, Toast.LENGTH_SHORT).show()
+        })
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent?.context)
+            .inflate(R.layout.row_custom_mainscreen_adapter, parent, false)
+
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        if (songDetails == null) {
+            return 0
+        } else {
+            return (songDetails as ArrayList<Songs>).size
+        }
+
+    }
+
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var trackTitle: TextView? = null
+        var trackArtist: TextView? = null
+        var contentHolder: RelativeLayout? = null
+
+        init {
+            trackTitle = view.findViewById(R.id.trackTitle) as TextView
+            trackArtist = view.findViewById(R.id.trackArtist) as TextView
+            contentHolder = view.findViewById(R.id.contentRow) as RelativeLayout
+        }
+    }
+}
--- Applications/02 Echo/app/src/main/java/com/example/echo/fragments/MainScreenFragment.kt
+++ Applications/02 Echo/app/src/main/java/com/example/echo/fragments/MainScreenFragment.kt
@@ -12,9 +12,12 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.echo.R
 import com.example.echo.Songs
+import com.example.echo.adapters.MainScreenAdapter
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -28,14 +31,16 @@ private const val ARG_PARAM2 = "param2"
  */
 class MainScreenFragment : Fragment() {
 
-    var nowPlayingBottomBar:RelativeLayout?=null
-    var playPauseButton: ImageButton?=null
-    var songTitle:TextView?=null
-    var visibleLayout:RelativeLayout?=null
-    var noSongs:RelativeLayout?=null
-    var recyclerView:RecyclerView?=null
-    var myActivity: Activity?=null
+    var getSongsList: ArrayList<Songs>? = null
+    var nowPlayingBottomBar:RelativeLayout? = null
+    var playPauseButton: ImageButton? = null
+    var songTitle:TextView? = null
+    var visibleLayout:RelativeLayout? = null
+    var noSongs:RelativeLayout? = null
+    var recyclerView:RecyclerView? = null
+    var myActivity: Activity? = null
 
+    var _mainScreenAdapter: MainScreenAdapter?=null
 
     override fun onCreateView(
         inflater: LayoutInflater?, container: ViewGroup?,
@@ -43,14 +48,24 @@ class MainScreenFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_main_screen, container, false)
 
-        visibleLayout = view?.findViewById<RelativeLayout>(R.id.visibleLayout)
-        noSongs = view?.findViewById<RelativeLayout>(R.id.noSongs)
-        nowPlayingBottomBar = view?.findViewById<RelativeLayout>(R.id.hiddenBarMainScreen)
-        songTitle = view?.findViewById<TextView>(R.id.songTitleMainScreen)
-        playPauseButton = view?.findViewById<ImageButton>(R.id.playPauseButton)
-        recyclerView = view?.findViewById<RecyclerView>(R.id.contentMain)
+        visibleLayout = view?.findViewById(R.id.visibleLayout)
+        noSongs = view?.findViewById(R.id.noSongs)
+        nowPlayingBottomBar = view?.findViewById(R.id.hiddenBarMainScreen)
+        songTitle = view?.findViewById(R.id.songTitleMainScreen)
+        playPauseButton = view?.findViewById(R.id.playPauseButton)
+        recyclerView = view?.findViewById(R.id.contentMain)
 
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        getSongsList = getSongsFromPhone()
+        _mainScreenAdapter = MainScreenAdapter(getSongsList as ArrayList<Songs>, myActivity as Context)
+        val mLayoutManager = LinearLayoutManager(myActivity)
+        recyclerView?.layoutManager = mLayoutManager
+        recyclerView?.itemAnimator = DefaultItemAnimator()
+        recyclerView?.adapter = _mainScreenAdapter
     }
 
     override fun onAttach(context: Context?) {
@@ -65,9 +80,11 @@ class MainScreenFragment : Fragment() {
 
     fun getSongsFromPhone():ArrayList<Songs>{
         var arrayList = ArrayList<Songs>()
+
         var contentResolver = myActivity?.contentResolver
         var songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         var songCursor = contentResolver?.query(songUri, null, null, null, null)
+
         if (songCursor!=null && songCursor.moveToFirst()){
             val songId = songCursor.getColumnIndex(MediaStore.Audio.Media._ID)
             val songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE)
@@ -83,11 +100,8 @@ class MainScreenFragment : Fragment() {
                 var currentDate = songCursor.getLong(dateIndex)
 
                 arrayList.add(Songs(currentId, currentTitle, currentArtist, currentData, currentDate))
-
-        }
+            }
         }
         return arrayList
     }
-
-
 }
