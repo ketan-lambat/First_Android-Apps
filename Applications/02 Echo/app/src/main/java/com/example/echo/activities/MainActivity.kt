cfd20cd6371a648ade73d4975902f91afbb59a5c
Ketan Lambat <ketan.lambat@ymail.com>
2019-10-08 00:06:38 +0530

    mainActivity.kt checked
    Signed-off-by: Ketan Lambat <ketan.lambat@ymail.com>

--- Applications/02 Echo/app/src/main/java/com/example/echo/activities/MainActivity.kt
+++ Applications/02 Echo/app/src/main/java/com/example/echo/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -15,6 +16,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.echo.R
 import com.example.echo.Songs
+import com.example.echo.activities.MainActivity.Staticated.drawerLayout
+import com.example.echo.activities.MainActivity.Staticated.notificationManager
 import com.example.echo.adapters.NavigationDrawerAdapter
 import com.example.echo.fragments.MainScreenFragment
 import com.example.echo.fragments.SongPlayingFragment
@@ -24,76 +27,80 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
 
     var navigationDrawerIconsList: ArrayList<String> = arrayListOf()
-    var images_for_navdrawer = intArrayOf(R.drawable.navigation_allsongs,
+    var images_for_navdrawer = intArrayOf(
+        R.drawable.navigation_allsongs,
         R.drawable.navigation_favorites,
         R.drawable.navigation_settings,
-        R.drawable.navigation_aboutus)
-    var trackNotificationBuilder: Notification?=null
-    object Staticated{
-        var drawerLayout:DrawerLayout?=null
-        var notificationManager:NotificationManager?=null
+        R.drawable.navigation_aboutus
+    )
+    var trackNotificationBuilder: Notification? = null
+
+    object Staticated {
+        var drawerLayout: DrawerLayout? = null
+        var notificationManager: NotificationManager? = null
     }
 
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
-        MainActivity.Statified.drawerLayout = findViewById(R.id.drawer_layout)
 
         navigationDrawerIconsList.add("All Songs")
         navigationDrawerIconsList.add("Favourites")
         navigationDrawerIconsList.add("Settings")
         navigationDrawerIconsList.add("About Us")
 
-        val toggle = ActionBarDrawerToggle(this@MainActivity,
-            MainActivity.Statified.drawerLayout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-
-        MainActivity.Statified.drawerLayout?.addDrawerListener(toggle)
-        toggle.syncState()
         val mainScreenFragment = MainScreenFragment()
         this.supportFragmentManager
             .beginTransaction()
             .add(R.id.details_fragment, mainScreenFragment, "MainScreenFragment")
             .commit()
 
-        val _navigationAdapter = NavigationDrawerAdapter(navigationDrawerIconsList,
-                                                    images_for_navdrawer,
-                                                    this)
+        drawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this@MainActivity,
+            drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout?.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val _navigationAdapter = NavigationDrawerAdapter(
+            navigationDrawerIconsList,
+            images_for_navdrawer,
+            this@MainActivity
+        )
         _navigationAdapter.notifyDataSetChanged()
-        _navigationAdapter.notifyDataSetChanged()
 
-        val navigation_recycler_view = findViewById<RecyclerView>(R.id.navigation_recycler_view)
-
-          //  LinearLayoutManager manager = new LinearLayoutManager(this)
-
-        navigation_recycler_view.layoutManager= LinearLayoutManager  (this)
+        val navigation_recycler_view = findViewById(R.id.navigation_recycler_view) as RecyclerView
+        navigation_recycler_view.layoutManager = LinearLayoutManager(this@MainActivity)
         navigation_recycler_view.itemAnimator = DefaultItemAnimator()
         navigation_recycler_view.adapter = _navigationAdapter
         navigation_recycler_view.setHasFixedSize(true)
 
         val intent = Intent(this@MainActivity, MainActivity::class.java)
-        val pIntent = PendingIntent.getActivity(this@MainActivity, System.currentTimeMillis().toInt(),
-                    intent, 0)
-        trackNotificationBuilder = Notification.Builder(this)
-            .setContentTitle("A track is playing in background")
-            .setSmallIcon(R.drawable.echo_logo)
-            .setContentIntent(pIntent)
-            .setOngoing(true)
-            .setAutoCancel(true)
-            .build()
+        val pIntent = PendingIntent.getActivity(
+            this@MainActivity, System.currentTimeMillis().toInt(),
+            intent, 0
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
+            trackNotificationBuilder = Notification.Builder(this)
+                .setContentTitle("A track is playing in background")
+                .setSmallIcon(R.drawable.echo_logo)
+                .setContentIntent(pIntent)
+                .setOngoing(true)
+                .setAutoCancel(true).build()
 
-        Statified.notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
     }
+}
 
     override fun onStart() {
         super.onStart()
         try {
-            Statified.notificationManager?.cancel(1978)
+            notificationManager?.cancel(1978)
         }catch (e:Exception){
             e.printStackTrace()
         }
@@ -103,7 +110,7 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         try {
             if (SongPlayingFragment.Statified.mediaPlayer?.isPlaying as Boolean){
-                Statified.notificationManager?.notify(1978, trackNotificationBuilder)
+                notificationManager?.notify(1978, trackNotificationBuilder)
             }
         }catch (e: Exception){
             e.printStackTrace()
@@ -113,7 +120,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         try {
-            Statified.notificationManager?.cancel(1978)
+            notificationManager?.cancel(1978)
         }catch (e:Exception){
             e.printStackTrace()
         }
